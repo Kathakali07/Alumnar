83cee2625024ee759961062c8e0cf7413551a14a
Tapabrata Gayen <tapabratagayen@gmail.com>
2024-11-24 23:38:31 +0530

    Center div

--- src/components/college_names/search.jsx
+++ src/components/college_names/search.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import "./search.css";
-import college from "./college_datas"
+import college from "./college_datas.jsx"
 import { Card } from "react-bootstrap";
 
 function Create_contact(college){
     return (
-        <Card
-            key = {college.id}
-            name = {college.name}
-            image = {college.image}
-            location = {college.location}
-        />
+        <Card key={college.id} className="my-3">
+            <Card.Img variant="top" src={college.image} alt={`${college.name} image`} />
+            <Card.Body>
+                <Card.Title>{college.name}</Card.Title>
+                <Card.Text>{college.location}</Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
 
@@ -22,10 +23,10 @@ function SearchBox() {
                 <h2>Search your College</h2>
                 <FaSearch id="search-icon" className="large-icon" /> {/* Fixed the className quote */}
                 <input type="search" placeholder="Search..." className="form-control" />
-                <button type="submit" className="btn btn-primar">Search</button> {/* Added margin-start */}
+                <button >Search</button> 
             </div>
 
-            <div className="college_names mt-10">
+            <div className="college_names">
                 <div className="college">
                     {college.map(Create_contact)}
                 </div>
